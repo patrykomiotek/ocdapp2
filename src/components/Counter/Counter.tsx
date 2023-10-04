6cded48c4263caf8e728faf52bd6682de4ad756c
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-04 11:29:00 +0200

    feat: trigger show counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  let counterValue = 0;
 
   useEffect(() => {
     const id = setInterval(() => {
       // setCount(count + 1);
-      setCount((value) => value + 1);
+      // setCount((value) => value + 1);
+      counterValue++;
     }, 1000);
 
     return () => {
@@ -14,9 +16,37 @@ export const Counter = () => {
     };
   }, []);
 
+  const handleClick = () => {
+    setCount(counterValue);
+  };
+
   return (
     <div>
       <span>{count}</span>
+      <button onClick={handleClick}>Show</button>
     </div>
   );
 };
+
+// import { useEffect, useState } from "react";
+
+// export const Counter = () => {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     const id = setInterval(() => {
+//       // setCount(count + 1);
+//       setCount((value) => value + 1);
+//     }, 1000);
+
+//     return () => {
+//       clearInterval(id);
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <span>{count}</span>
+//     </div>
+//   );
+// };
