e2c542a4a6fa35e38dc75bf9b838637f618f9eb9
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-04 16:48:58 +0200

    feat: with axios

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const BASE_URL = "https://api.airtable.com/v0/appJ0votvrhmT0Sbq";
 const TOKEN =
   "Bearer patVQ864y6eGr1CkQ.760719abde62320306c65daff180b15fdc943d7cbf5301dc552175a39969fa4f";
@@ -8,7 +10,19 @@ export interface CreateProductDto {
   description: string;
 }
 
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: TOKEN,
+    "content-type": "application/json",
+  },
+});
+
 export const createProduct = (data: CreateProductDto): Promise<Response> => {
+  return api.post<Product>(`/products`, {
+    records: data,
+  });
+
   return fetch(`${BASE_URL}/products`, {
     headers: {
       Authorization: TOKEN,
