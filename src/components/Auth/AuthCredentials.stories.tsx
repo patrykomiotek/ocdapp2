5deee2a689241c3332b89119860584b3635c4ce5
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-04 14:17:20 +0200

    feat: story for AuthCredentials

---
+++ src/components/Auth/AuthCredentials.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { AuthCredentials } from "./AuthCredentials";
+import { AuthProvider } from "./AuthContext";
+
+const meta = {
+  title: "Components/AuthCredentials",
+  component: AuthCredentials,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof AuthCredentials>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// TODO: this component needs wrapper to pass refs logic
+export const Primary = () => (
+  <AuthProvider>
+    <AuthCredentials />
+  </AuthProvider>
+);
