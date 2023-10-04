22f6799eb3e7e9af31e31ebf736462bdf89680f8
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-04 16:41:40 +0200

    feat: creating new product

---
+++ src/components/Products/CreateProduct.tsx
@@ -0,0 +1,40 @@
+import { FormEventHandler, useRef } from "react";
+import { Input } from "../../ui/molecules/Input/Input";
+import { CreateProductDto, createProduct } from "../../services/products";
+
+export const CreateProduct = () => {
+  const nameFieldRef = useRef<HTMLInputElement>(null);
+  const priceFieldRef = useRef<HTMLInputElement>(null);
+  const descriptionFieldRef = useRef<HTMLInputElement>(null);
+
+  const save = async (data: CreateProductDto) => {
+    try {
+      const response = await createProduct(data);
+      // const responseData = await response.json() as ;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+
+    const product = {
+      name: nameFieldRef.current?.value || "",
+      price: parseInt(priceFieldRef.current?.value || "", 10) || 0,
+      description: descriptionFieldRef.current?.value || "",
+    };
+
+    void save(product);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input type="text" label="Name" ref={nameFieldRef} />
+        <Input type="number" label="Price" ref={priceFieldRef} />
+        <Input type="text" label="Description" ref={descriptionFieldRef} />
+      </form>
+    </div>
+  );
+};
---
+++ src/services/products.ts
@@ -0,0 +1,22 @@
+const BASE_URL = "https://api.airtable.com/v0/appJ0votvrhmT0Sbq";
+const TOKEN =
+  "Bearer patVQ864y6eGr1CkQ.760719abde62320306c65daff180b15fdc943d7cbf5301dc552175a39969fa4f";
+
+export interface CreateProductDto {
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const createProduct = (data: CreateProductDto): Promise<Response> => {
+  return fetch(`${BASE_URL}/products`, {
+    headers: {
+      Authorization: TOKEN,
+      "content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      records: data,
+    }),
+  });
+};
