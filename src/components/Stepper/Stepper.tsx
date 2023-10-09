bea045236f22346d4631c8a2c866221031d1f370
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-09 16:41:57 +0200

    feat: stepper component with useReducer

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,17 +1,49 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+  count: 0,
+};
+
+type State = typeof initialState;
+
+// action:
+// {
+// type: string,
+// payload?: Record
+// }
+
+enum ActionType {
+  INCREMENT = "INCREMENT",
+  DECREMENT = "DECREMENT",
+}
+
+type Action = {
+  type: ActionType;
+  payload?: Record<string, string>;
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT:
+      return { count: state.count + 1 };
+    case ActionType.DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const decrement = { type: ActionType.DECREMENT };
+const increment = { type: ActionType.INCREMENT };
 
 export const Stepper = () => {
-  const [state, setState] = useState({ count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <button onClick={() => setState((value) => ({ count: value.count - 1 }))}>
-        -
-      </button>
+      <button onClick={() => dispatch(decrement)}>-</button>
       <span>{state.count}</span>
-      <button onClick={() => setState((value) => ({ count: value.count + 1 }))}>
-        +
-      </button>
+      <button onClick={() => dispatch(increment)}>+</button>
     </div>
   );
 };
